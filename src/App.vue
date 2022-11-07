81d5de45edd67e96c8d62ebfba54b82de3afd4ab
sder12 <sara.cetto@gmail.com>
2022-11-07 15:29:33 +0100

    AppTitle done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  components: {
+    AppTitle,
+  }
+}
 </script>
 
 <template>
+
+  <AppTitle />
+
 </template>
 
 <style scoped>
