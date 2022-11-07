026c74f370bdc8542775d2a0bcb6ae7a387b0151
sder12 <sara.cetto@gmail.com>
2022-11-07 15:31:57 +0100

    Hero placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 export default {
   components: {
     AppTitle,
+    AppHero,
   }
 }
 </script>
@@ -10,6 +12,7 @@ export default {
 <template>
 
   <AppTitle />
+  <AppHero />
 
 </template>
 
--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+    <p>Hero</p>
 </template>
 
 <style scoped>
